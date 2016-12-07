ee320689d1792a85a65f2b663149e034d4d7a2e1
Rami Bitar <Rami Bitar>
2016-12-06 20:37:36 -0500

    added whittaker classification plot. need coloring based on ranks, explained in comment
    Signed-off-by: Rami Bitar <Rami Bitar>

--- ProjectCeres/ProjectCeres/Biomes.cs
+++ ProjectCeres/ProjectCeres/Biomes.cs
@@ -13,7 +13,7 @@ namespace ProjectCeres
         //cutoff determines the amount of steps before moisture reaches its lowest point
         public static RectGrid moisture(RectGrid input, Project proj, int cutoff)
         {
-            
+
             GeoGrid geo = new GeoGrid(proj.Frequency);
             GeoGrid output = new GeoGrid(proj.Frequency);
             RectGrid coastDist = new RectGrid(input.Height, input.Width);
@@ -31,7 +31,7 @@ namespace ProjectCeres
                 //Wheeeeeeee~
                 for (int par = 0; par < GeoGrid.NUMPARA; par++)
                 {
-                    for (int r = 0; r < freq-1; r++)
+                    for (int r = 0; r < freq - 1; r++)
                     {
                         for (int c = 0; c < 2 * freq - 1; c++)
                         {
@@ -47,7 +47,7 @@ namespace ProjectCeres
                                         //Remove from next step
                                         next.getTile(par, r, c).Value = 0f;
                                         //set distance away from coast
-                                        output.getTile(par, r, c).Rank = Math.Min(rank,cutoff);
+                                        output.getTile(par, r, c).Rank = Math.Min(rank, cutoff);
                                         clear = false;
                                     }
                                 }
@@ -61,7 +61,7 @@ namespace ProjectCeres
             //Determine moisture weight by dividing by cutoff (yes, I coulda put this in the loop above)
             for (int par = 0; par < GeoGrid.NUMPARA; par++)
             {
-                for (int r = 0; r < freq-1; r++)
+                for (int r = 0; r < freq - 1; r++)
                 {
                     for (int c = 0; c < 2 * freq - 1; c++)
                     {
@@ -82,7 +82,7 @@ namespace ProjectCeres
             {
                 for (int c = 0; c < input.Width; c++)
                 {
-                    if(input.getTile(r,c).Value<=sea)
+                    if (input.getTile(r, c).Value <= sea)
                     {
                         landForms.getTile(r, c).Value = 0f;
                     }
@@ -101,9 +101,9 @@ namespace ProjectCeres
             GeoGrid landForms = new GeoGrid(proj.Frequency);
             for (int par = 0; par < GeoGrid.NUMPARA; par++)
             {
-                for (int r = 0; r < freq-1; r++)
+                for (int r = 0; r < freq - 1; r++)
                 {
-                    for (int c = 0; c < 2*freq-1; c++)
+                    for (int c = 0; c < 2 * freq - 1; c++)
                     {
                         if (input.getTile(par, r, c).Value <= sea)
                         {
@@ -117,15 +117,15 @@ namespace ProjectCeres
                         {
                             landForms.getTile(par, r, c).Value = 1f;
                         }
-                        else if (r == 0 && c == freq-1)
+                        else if (r == 0 && c == freq - 1)
                         {
                             landForms.getTile(par, r, c).Value = 1f;
                         }
-                        else if (r == 0 && c == 2*freq - 2)
+                        else if (r == 0 && c == 2 * freq - 2)
                         {
                             landForms.getTile(par, r, c).Value = 1f;
                         }
-                        else if (r == freq-1 && c == 0)
+                        else if (r == freq - 1 && c == 0)
                         {
                             landForms.getTile(par, r, c).Value = 1f;
                         }
@@ -152,22 +152,116 @@ namespace ProjectCeres
             float dist;
             float latTemp;
             float altTemp;
-            RectGrid temp = new RectGrid(input.Height,input.Width);
-            for(int row = 0; row < input.Height; row++)
+            RectGrid temp = new RectGrid(input.Height, input.Width);
+            for (int row = 0; row < input.Height; row++)
             {
-                for(int col = 0; col < input.Width; col++)
+                for (int col = 0; col < input.Width; col++)
                 {
-                    dist = (float)(gridEquator - row)/(input.Height/2);
+                    dist = (float)(gridEquator - row) / (input.Height / 2);
                     latTemp = 1 - ((float)Math.Sqrt(Math.Abs(dist)));
                     altTemp = MAXALTIMPACT * input.getTile(row, col).Value;
-                    temp.getTile(row, col).Value = Math.Max(0,latTemp-altTemp);
+                    temp.getTile(row, col).Value = Math.Max(0, latTemp - altTemp);
                 }
             }
             return temp;
         }
+
         public static RectGrid Temperature(RectGrid input)
         {
-            return Temperature(input,0.5f);
+            return Temperature(input, 0.5f);
+        }
+
+        public static RectGrid BiomeMap(RectGrid input, Project proj, int cutoff)
+        {
+            RectGrid moistureGrid = Biomes.moisture(input, proj, cutoff);
+            RectGrid tempGrid = Biomes.Temperature(input);
+            RectGrid biomeGrid = new RectGrid(input.Height, input.Width);
+
+
+            //RANKING SYSTEM, NO ICE
+            //1:tundra
+            //2:grassland
+            //3:woodland
+            //4:boreal forest
+            //5:seasonal forest
+            //6:temperate rainforest
+            //7:tropical rainforest
+            //8:savanna
+            //9:desert
+
+            for (int row = 0; row < input.Height; row++)
+            {
+                for (int col = 0; col < input.Width; col++)
+                {
+                    //DRYER Moisture
+                    if (moistureGrid.getTile(row, col).Value < .2)
+                    {
+                        if (tempGrid.getTile(row, col).Value < .2)
+                            biomeGrid.getTile(row, col).Rank = 1;
+                        else if (tempGrid.getTile(row, col).Value >= .2 & tempGrid.getTile(row, col).Value < .4)
+                            biomeGrid.getTile(row, col).Rank = 2;
+                        else if (tempGrid.getTile(row, col).Value >= .4)
+                            biomeGrid.getTile(row, col).Rank = 9;
+
+                    }
+
+                    //DRY Moisture
+                    if (moistureGrid.getTile(row, col).Value >= .2 & moistureGrid.getTile(row, col).Value < .4)
+                    {
+                        if (tempGrid.getTile(row, col).Value < .2)
+                            biomeGrid.getTile(row, col).Rank = 1;
+                        else if (tempGrid.getTile(row, col).Value >= .2 & tempGrid.getTile(row, col).Value < .6)
+                            biomeGrid.getTile(row, col).Rank = 3;
+                        else if (tempGrid.getTile(row, col).Value >= .6)
+                            biomeGrid.getTile(row, col).Rank = 8;
+
+                    }
+
+                    //WET Moisture 
+                    if (moistureGrid.getTile(row, col).Value >= .4 & moistureGrid.getTile(row, col).Value < .6)
+                    {
+                        if (tempGrid.getTile(row, col).Value < .2)
+                            biomeGrid.getTile(row, col).Rank = 1;
+                        else if (tempGrid.getTile(row, col).Value >= .2 & tempGrid.getTile(row, col).Value < .4)
+                            biomeGrid.getTile(row, col).Rank = 4;
+                        else if (tempGrid.getTile(row, col).Value >= .4 & tempGrid.getTile(row, col).Value < .6)
+                            biomeGrid.getTile(row, col).Rank = 3;
+                        else if (tempGrid.getTile(row, col).Value >= .6)
+                            biomeGrid.getTile(row, col).Rank = 8;
+
+                    }
+
+                    //WETTER Moisture 
+                    if (moistureGrid.getTile(row, col).Value >= .6 & moistureGrid.getTile(row, col).Value < .8)
+                    {
+                        if (tempGrid.getTile(row, col).Value < .2)
+                            biomeGrid.getTile(row, col).Rank = 1;
+                        else if (tempGrid.getTile(row, col).Value >= .2 & tempGrid.getTile(row, col).Value < .4)
+                            biomeGrid.getTile(row, col).Rank = 4;
+                        else if (tempGrid.getTile(row, col).Value >= .4 & tempGrid.getTile(row, col).Value < .6)
+                            biomeGrid.getTile(row, col).Rank = 5;
+                        else if (tempGrid.getTile(row, col).Value >= .6)
+                            biomeGrid.getTile(row, col).Rank = 7;
+
+                    }
+
+                    //WETTEST Moisture 
+                    if (moistureGrid.getTile(row, col).Value >= .8)
+                    {
+                        if (tempGrid.getTile(row, col).Value < .2)
+                            biomeGrid.getTile(row, col).Rank = 1;
+                        else if (tempGrid.getTile(row, col).Value >= .2 & tempGrid.getTile(row, col).Value < .4)
+                            biomeGrid.getTile(row, col).Rank = 4;
+                        else if (tempGrid.getTile(row, col).Value >= .4 & tempGrid.getTile(row, col).Value < .6)
+                            biomeGrid.getTile(row, col).Rank = 6;
+                        else if (tempGrid.getTile(row, col).Value >= .6)
+                            biomeGrid.getTile(row, col).Rank = 7;
+
+                    }
+                }
+            }
+
+            return biomeGrid;
         }
     }
 }
