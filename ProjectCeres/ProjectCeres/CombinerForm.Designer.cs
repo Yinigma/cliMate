45fd19695ee5780bffe515181b11caaed1cb25b9
Ben 'Kenobi' Denison <bdenison@purdue.edu>
2016-12-07 17:48:21 -0500

    Season displays added

---
+++ ProjectCeres/ProjectCeres/CombinerForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ProjectCeres
+{
+    partial class CombinerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combineBox = new System.Windows.Forms.ComboBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // combineBox
+            // 
+            this.combineBox.FormattingEnabled = true;
+            this.combineBox.Items.AddRange(new object[] {
+            "Average",
+            "Add",
+            "Subtract",
+            "Multiply",
+            "Divide",
+            "Min",
+            "Max"});
+            this.combineBox.Location = new System.Drawing.Point(61, 38);
+            this.combineBox.Name = "combineBox";
+            this.combineBox.Size = new System.Drawing.Size(157, 21);
+            this.combineBox.TabIndex = 0;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(143, 85);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(61, 85);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // CombinerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 120);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.combineBox);
+            this.Name = "CombinerForm";
+            this.Text = "Select mode";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combineBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+    }
+}
---
+++ ProjectCeres/ProjectCeres/CombinerForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectCeres
+{
+    public partial class CombinerForm : Form
+    {
+        public CombinerForm(CombinerNode node)
+        {
+            InitializeComponent();
+            combineBox.SelectedIndex = CombinerNode.AVERAGE;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
